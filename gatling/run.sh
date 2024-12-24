b9bd6193a3805c9621975199d217db0b6af708cf
Arnaldo Bertoni Jr <arnaldo.bertoni.jr@gmail.com>
2024-12-24 10:39:08 -0300

    gatling setup

---
+++ gatling/run.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env sh
+
+CURRENT_PATH=$(dirname $(readlink -m "$0"))
+
+GATLING_BIN_DIR="$CURRENT_PATH/deps/gatling/bin"
+
+WORKSPACE=$CURRENT_PATH
+
+sh $GATLING_BIN_DIR/gatling.sh -rm local -s RinhaBackendSimulation \
+    -rd "DESCRICAO" \
+    -rf $WORKSPACE/user-files/results \
+    -sf $WORKSPACE/user-files/simulations \
+    -rsf $WORKSPACE/user-files/resources \
+
+sleep 3
+
+curl -v "http://localhost:9999/contagem-pessoas"
---
+++ makefile
@@ -0,0 +1,5 @@
+install-gatling:
+	./gatling/install.sh
+
+run-gatling:
+	./gatling/run.sh
